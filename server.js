fb0883a3629bd9f22662c3844fb86fec0034228b
Kaliandra de Freitas <dev.kaliandra.freitas@gmail.com>
2024-09-12 19:46:10 -0400

    Subindo projeto completo

---
+++ server.js
@@ -0,0 +1,30 @@
+// import { createServer } from 'node:http'
+
+// const server = createServer((request, response)=> {
+//     response.write('Hello world');
+
+//     return response.end()
+// });
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+    server.post('/videos', () => {
+        return 'Hello World'
+    });
+
+    server.get('/videos', () => {
+        return 'Hello World'
+    });
+
+    server.put('/videos', () => {
+        return 'Hello World'
+    });
+    
+
+server.listen({
+    port: 3333,
+})
